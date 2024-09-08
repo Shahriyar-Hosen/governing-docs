4d6f1949a030637662beb435c5e32d17b36009f3
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-09-08 21:43:10 +0600

    faq done

--- src/components/home/faq.tsx
+++ src/components/home/faq.tsx
@@ -10,12 +10,12 @@ interface IFaqCard {
 }
 const FaqCard: FC<IFaqCard> = ({ a, q, isActive, onClick }) => {
   return (
-    <div className="shadow-faq w-full rounded-[20px] px-[30px] py-5 bg-white">
+    <div className="shadow-faq w-full rounded-[20px] px-5 md:px-[30px] py-5 bg-white">
       <button
         onClick={onClick}
-        className="w-full inline-flex justify-between items-center"
+        className="w-full inline-flex justify-between items-center gap-5"
       >
-        <h1 className="font-bold text-[26px] leading-[36.4px] text-accent">
+        <h1 className="font-bold text-start text-lg lg:text-[26px] lg:leading-[36.4px] text-accent">
           {q}
         </h1>
 
@@ -24,7 +24,9 @@ const FaqCard: FC<IFaqCard> = ({ a, q, isActive, onClick }) => {
         </div>
       </button>
       {isActive && (
-        <p className="tracking-[2%] text-accent max-w-[807px] pt-5">{a}</p>
+        <p className="tracking-[2%] text-sm sm:text-base text-accent max-w-[807px] pt-5">
+          {a}
+        </p>
       )}
     </div>
   );
@@ -43,7 +45,7 @@ export const Faq = () => {
 
   return (
     <section className="w-full mt-[150px] relative ">
-      <div className="w-full max-w-[982px] mx-auto">
+      <div className="w-full max-w-[982px] mx-auto px-2.5">
         <SectionTitle gradient="Asked Questions">Frequently </SectionTitle>
 
         <div className="pt-10 space-y-5 relative z-10">
