7c5fd3a7176218292af98797c05a0d6ed081736d
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-09-08 21:10:42 +0600

    nevar done

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -5,7 +5,7 @@ export const Button: FC<IClassChildren> = ({ children, className }) => {
   return (
     <button
       className={cn(
-        "font-bold py-4 px-[25px] bg-gradient-primary opacity-90  transition-all delay-150 hover:opacity-100 text-white  rounded-full",
+        "font-bold py-4 px-[25px] bg-gradient-primary opacity-90 transition-all delay-150 hover:opacity-100 text-white rounded-full text-sm md:text-base",
         className
       )}
     >
--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -3,17 +3,17 @@ import { Button, Container } from "~/common";
 
 export const Navbar = () => {
   return (
-    <nav className="py-11">
-      <Container className="px-[26px]">
-        <div className="flex justify-between items-center gap-2.5 px-[30px] py-5 bg-white rounded-full">
-          <h1 className="font-space-grotesk text-4xl font-bold">
+    <nav className="py-8 xl:py-11">
+      <Container className="px-5 xl:px-[26px]">
+        <div className="flex justify-between items-center gap-2.5 px-5 xl:px-[30px] py-3 xl:py-5 bg-white rounded-full">
+          <h1 className="font-space-grotesk text-2xl lg:text-3xl xl:text-4xl font-bold">
             GoverningDocs
           </h1>
 
-          <div className="flex justify-center items-center gap-4">
+          <div className="hidden lg:flex justify-center items-center gap-3 xl:gap-4">
             {nevItems.map(({ href, label }, i) => (
               <a href={href} key={i}>
-                <p className="text-accent text-base">{label}</p>
+                <p className="text-accent text-sm xl:text-base">{label}</p>
               </a>
             ))}
           </div>
