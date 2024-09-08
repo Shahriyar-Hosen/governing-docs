5fa04f418616bb367e6ad7de62adb6ca702ec141
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-09-08 20:57:37 +0600

    clean up

--- src/components/home/note.tsx
+++ src/components/home/note.tsx
@@ -2,7 +2,7 @@ import { Button, Container } from "../common";
 
 export const Note = () => {
   return (
-    <section className="pt-[150px]">
+    <section className="pt-[150px] px-2.5">
       <Container className="bg-gradient-primary rounded-[20px] py-14 pl-10 pr-16 flex justify-between items-center gap-2.5">
         <div className="space-y-[30px]">
           <h1 className="font-bold text-[46px] leading-[64.4px] text-white  max-w-[566px]">
--- src/components/home/partners.tsx
+++ src/components/home/partners.tsx
@@ -1,20 +1,22 @@
-import { SectionTitle } from "~/common";
+import { Container, SectionTitle } from "~/common";
 
 export const Partners = () => {
   return (
-    <section className="bg-[#E6F9FD] rounded-[20px] py-[50px] w-full max-w-[1276px] px-3 mx-auto">
-      <SectionTitle gradient="Partners">Our</SectionTitle>
+    <section className="px-2.5">
+      <Container className="max-w-[1276px] px-3 py-[50px] bg-[#E6F9FD] rounded-[20px]">
+        <SectionTitle gradient="Partners">Our</SectionTitle>
 
-      <div className="flex flex-wrap justify-center items-center gap-x-[30px] gap-y-7 pt-10">
-        {partners.map(({ src, alt }) => (
-          <div
-            key={src}
-            className="w-[274px] bg-white rounded-xl h-[101px] flex justify-center items-center border border-[#D6D6D6]"
-          >
-            <img src={src} alt={alt} />
-          </div>
-        ))}
-      </div>
+        <div className="flex flex-wrap justify-center items-center gap-x-[30px] gap-y-7 pt-10">
+          {partners.map(({ src, alt }) => (
+            <div
+              key={src}
+              className="w-[274px] bg-white rounded-xl h-[101px] flex justify-center items-center border border-[#D6D6D6]"
+            >
+              <img src={src} alt={alt} />
+            </div>
+          ))}
+        </div>
+      </Container>
     </section>
   );
 };
